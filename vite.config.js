50c41560a6f5581635faecca52f7b9fb5d25bd6b
vgabriel-lira <vgabriel.lira@gmail.com>
2025-11-18 16:58:55 -0300

    vite.config.js correção

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-
-  base: './', 
-  
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    host: true
   }
 });
