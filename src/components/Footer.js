6b3d2a8d028f25fef9575c9ade44cde668bb6eca
vgabriel-lira <vgabriel.lira@gmail.com>
2026-01-23 16:12:36 -0300

    footer/header no layout

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="informacoes">
+        <p>© 2025 Jaguaribe Decor.</p>
+        <div className="social-media">
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-instagram"></i></a>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-facebook"></i></a>
+          <a href="https://pinterest.com" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-pinterest"></i></a>
+        </div>
+      </div>
+    </footer>
+  );
+}
